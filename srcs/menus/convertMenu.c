850289300ab064206e33460cbe6a10d7bb4de575
QuentinHelion <helion.quentin@gmail.com>
2022-12-19 22:08:11 +0100

    functionnal convertor menu

--- srcs/menus/convertMenu.c
+++ srcs/menus/convertMenu.c
@@ -1,7 +1,26 @@
 #include "app.h"
 
 int convertMenu(){
+  char ip[16];
+  char buffer[16];
+  // ip_addr addr;
 
+  printf("\n========oOo========\n");
+
+  do {
+    printf("Please enter IP: ");
+    fgets(ip, 16, stdin);
+    fgetc(stdin);
+    printf("%s\n", ip);
+    for (short i = 0; i < 16; i++) {
+      buffer[i] = ip[i];
+    }
+  } while(checkIpValid(ip));
+
+  printf("%s\n", buffer);
+
+  binaryIP(ipToStruct(buffer, "24"));
+  hexaIP(ipToStruct(buffer, "24"));
 
   return EXIT_SUCCESS;
 }
