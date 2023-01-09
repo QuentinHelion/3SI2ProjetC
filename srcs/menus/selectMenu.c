552abdf199cb10c77710f028fb18da2e572fbbaa
QuentinHelion <helion.quentin@gmail.com>
2023-01-09 10:38:28 +0100

    clean code select menu

--- srcs/menus/selectMenu.c
+++ srcs/menus/selectMenu.c
@@ -3,15 +3,13 @@
 int selectMenu(MYSQL *conn){
   WINDOW *w;
   w = newwin( 50, 40, 1, 1 ); // create a new window
-  // box( w, 0, 0 ); // sets default borders for the window
 
-  // printw("%s",THEME_BAR_TOP);
   selectIP("1", "1", conn);
 
   exitMenu();
 
   clear();
   delwin( w );
-  
+
   return EXIT_SUCCESS;
 }
