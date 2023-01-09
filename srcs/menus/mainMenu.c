3ccbef30431bc8a050d524ae1206965a2059f3dd
QuentinHelion <helion.quentin@gmail.com>
2023-01-09 10:32:47 +0100

    clean code mainMenu

--- srcs/menus/mainMenu.c
+++ srcs/menus/mainMenu.c
@@ -1,40 +1,6 @@
 #include "app.h"
 
 int mainMenu(){
-  // short choice = 0;
-  // // char *str;
-  //
-  // do {
-  //   // printf("%s",THEME_BAR_TOP);
-  //   // printf("1: Show all saved IP\n");
-  //   // printf("2: Add IP\n");
-  //   // printf("3: Convert IP\n");
-  //   // printf("4: Filter saved IP with mask\n");
-  //   // printf("5: Delete saved IP\n");
-  //   // printf("0: Exit\n");
-  //   // printf("What do you want to do ?\n");
-  //
-  //   initscr();              // Initialise la structure WINDOW et autres paramètres
-  //   printw("%s",THEME_BAR_TOP);
-  //   printw("1: Show all saved IP\n");
-  //   printw("2: Add IP\n");
-  //   printw("3: Convert IP\n");
-  //   printw("4: Filter saved IP with mask\n");
-  //   printw("5: Delete saved IP\n");
-  //   printw("0: Exit\n");
-  //   // endwin();
-  //   // printw("What do you want to do ?\n");
-  //   refresh();              // Rafraîchit la fenêtre courante afin de voir le message apparaître
-  //   // getchstr(str);                // On attend que l'utilisateur appui sur une touche pour quitter
-  //
-  //   scanw("%hd",&choice);
-  //   // fgetc(stdin);
-  //   // choice = strToInt(str);
-  // } while(choice > 5|| choice < 0);
-  //
-  // clear();
-  // // endwin();               // Restaure les paramètres par défaut du terminal
-
   WINDOW *w;
   // int LENGTH = 6;
   // int MAX_LETTER = 35;
@@ -91,8 +57,6 @@ int mainMenu(){
 
   clear();
   delwin( w );
-  // printf("%s\n", list[i]);
-
 
   return i+1;
 }
