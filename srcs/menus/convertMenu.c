0a02d71da16bfdc322644ecdf29a8c887ff748c0
QuentinHelion <helion.quentin@gmail.com>
2023-01-09 10:34:47 +0100

    clean code convertMenu

--- srcs/menus/convertMenu.c
+++ srcs/menus/convertMenu.c
@@ -4,16 +4,12 @@ int convertMenu(){
   char ip[16];
   char buffer[16];
 
-  // printf("%s",THEME_BAR_TOP);
   WINDOW *w;
   w = newwin( 50, 40, 1, 1 ); // create a new window
   nodelay(stdscr, FALSE);
   echo();
 
   do {
-    // printf("Please enter IP: ");
-    // fgets(ip, 16, stdin);
-    // fgetc(stdin);
     printw("Please enter IP: ");
     scanw("%s",ip);
     for (short i = 0; i < 16; i++) {
@@ -22,16 +18,13 @@ int convertMenu(){
   } while(checkIpValid(ip)); // if entry is good we can leave the loop
 
   // === Call convertion function ===
-  // printf("\nBinary: ");
   printw("\nBinary: ");
   binaryIP(ipToStruct(buffer, "24")); // Binary convertion
-  // printf("\nHexa: ");
   printw("\nHexa: ");
   hexaIP(ipToStruct(buffer, "24")); // Hexa convertion
 
   // === Show ip type ===
   // Public, private, special, loopback, apipa
-  // printf("\nIP Type: ");
   printw("\nIP Type: ");
   ipType(ipToStruct(buffer, "24"));
 
